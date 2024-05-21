709e9de57d322e03ee0f4787d36652c75f45bbc9
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-05-21 08:00:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert(){
+    function tampil(){
+        alert("Vina Rahma Vionita (056), Andini Septi Andri(053), Safira Nurul Hidayah(054)")
+    }
+    tampil(); // Memanggil fungsi tampil()
+}
